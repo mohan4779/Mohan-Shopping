1e68b809790eaed3355c17a57eaa7ac48c925283
mohan4779 <139194778+mohan4779@users.noreply.github.com>
2023-12-19 11:24:54 +0530

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,68 @@
+import product1 from "./assets/1.jpg";
+import product2 from "./assets/2.jpg";
+import product3 from "./assets/9.jpg";
+import product4 from "./assets/4.jpg";
+import product5 from "./assets/10.jpg";
+import product6 from "./assets/6.jpg";
+import product7 from "./assets/7.jpg";
+import product8 from "./assets/8.jpg";
+
+export const PRODUCTS = [
+    {
+        id : 1,
+        productName : "IPhone",
+        price : 80000,
+        productImage: product1,
+
+    },
+    {
+        id :2, 
+        productName : "Samsung S22",
+        price : 85000,
+        productImage: product2,
+    },
+
+    {
+        id : 3,
+        productName : "biegert & funk Watch",
+        price : 90000,
+        productImage: product3,
+    },
+
+    {
+        id : 4,
+        productName :"HP Probook" ,
+        price : 59000,
+        productImage: product4,
+    },
+
+    {
+        id : 5,
+        productName : "Sweatshirt",
+        price : 1900,
+        productImage: product5,
+    },
+
+    {
+        id : 6,
+        productName : "Formal Shoes",
+        price : 2000,
+        productImage: product6,
+    },
+
+    {
+        id :7, 
+        productName : "Canon camera",
+        price : 40000,
+        productImage: product7,
+    },
+
+    {
+        id : 8,
+        productName : "Neck band",
+        price : 1500,
+        productImage: product8,
+    },
+
+     
+];
